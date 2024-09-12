0559d0f5eef7d50f6472e2739b613cd4f46b72b8
rishabrath31 <rishabrath319@gmail.com>
2024-09-12 09:39:53 +0530

    Added New Files

---
+++ oops.js
@@ -0,0 +1,15 @@
+// Object Literal
+//
+
+const user = {
+  names: ["Rishab", "Anisha"],
+  age: 23,
+  printDetails: function () {
+    console.log("Details of User: ");
+  },
+  greet: function () {
+    console.log("Greetings!!");
+  },
+};
+
+console.log(user.names);
